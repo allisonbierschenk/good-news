f187073827a8080aa6d245b90ca4a400e28e3bf3
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-01-26 13:38:56 -0600

    changed the layout of the html page and added sports content from api

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 
-// const domain = 'https://api.thenewsapi.com/v1/news/all?'
-let params = {
-  api_token: 'lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw',
-  categories: 'food,travel,entertainment,sports',
-  language: 'en',
-  limit: 5
-}
+
 
 const getSports = async () => {
+  
+  let sports = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw&language=en&categories=sports')
+  console.log(sports.data.data)
+
   try {
-    let sports = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw&language=en&categories=sports')
-    // console.log(sports)
+    sports.data.data.forEach(sportArticle => {
+      let container = document.querySelector('.categories')
+      let articleContainer = document.createElement('div')
+      container.append(articleContainer)
+      
+      let title = document.createElement('h1')
+      title.textContent = sportArticle.title
+      articleContainer.append(title)
+      
+      let description = document.createElement('p')
+      description.textContent = sportArticle.description
+      articleContainer.append(description)
+
+      let image = document.createElement('img')
+      image.scr = sportArticle.image_url
+      articleContainer.append(image)
+      
+      let url = document.createElement('url')
+      url.textContent = sportArticle.url
+      articleContainer.append(url)
+      
+    })
   } catch (error) {
     console.log(error)
   }
 }
+
 getSports()
+
+let sportButton = document.querySelector('.sports')
+sportButton.addEventListener('click', getSports)

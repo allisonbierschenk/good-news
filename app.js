634aaf67f3e671c6b590499d35bab289d9b60e1d
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-01-26 12:45:36 -0600

    changed structure to have paramaters in the url and separating by category

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-
-const domain = 'https://api.thenewsapi.com/v1/news/all?'
+// const domain = 'https://api.thenewsapi.com/v1/news/all?'
 let params = {
   api_token: 'lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw',
   categories: 'food,travel,entertainment,sports',
@@ -8,20 +7,12 @@ let params = {
   limit: 5
 }
 
-const getTheNews = async () => {
+const getSports = async () => {
   try {
-    let requestResponse = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw')
-    // console.log(response.data)
-    let esc = encodeURIComponent
-    let articles = Object.keys(params).map(function(k) {return esc(k) + '=' + esc(params[k]);})
-      .join('&');
-    fetch("https://api.thenewsapi.com/v1/news/all?" + articles, requestResponse)
-      .then(response => response.text())
-      // .then(result => console.log(result))
-    //https://www.thenewsapi.com/documentation
-    console.log(articles)
+    let sports = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw&language=en&categories=sports')
+    // console.log(sports)
   } catch (error) {
     console.log(error)
   }
 }
-getTheNews()
+getSports()

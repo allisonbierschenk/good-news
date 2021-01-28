437b833fb3a21d13f92805293cc797afcced31a5
Allison Reynolds <allisonreynolds@Allisons-Air.attlocal.net>
2021-01-27 20:18:03 -0600

    updated all buttons with styling

--- app.js
+++ app.js
@@ -9,27 +9,33 @@ const getSports = async () => {
       let articleContainer = document.createElement('div')
       articleContainer.classList.add('article-container')
       container.append(articleContainer)
+
       let image = document.createElement('img')
       image.src = sportArticle.image_url
       image.classList.add('image-custom')
       articleContainer.append(image)
+
       let title = document.createElement('h1')
       title.textContent = sportArticle.title
       title.classList.add('title-custom')
       articleContainer.append(title)
+
       let description = document.createElement('p')
       description.textContent = sportArticle.description
       description.classList.add('description-custom')
       articleContainer.append(description)
+
       let button = document.createElement('button')
       button.classList.add('button-style')
       articleContainer.append(button)
+
       let a = document.createElement('a')
       a.classList.add('readmore-button-text')
       a.href = sportArticle.url
       let link = document.createTextNode('Read More')
       button.append(a)
       a.appendChild(link)
+
   })
 } catch (error) {
     console.log(error)
@@ -41,8 +47,10 @@ const removeArticles = () => {
     articleRemove.removeChild(articleRemove.lastChild)
   }
 }
-  let sportButton = document.querySelector('.sports')
-  sportButton.addEventListener('click', getSports)
+let sportButton = document.querySelector('.sports')
+sportButton.addEventListener('click', getSports)
+  
+
   //ENTERTAINMENT
   const getEntertainment = async () => {
     let entertainment = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw&language=en&categories=entertainment')
@@ -50,28 +58,45 @@ const removeArticles = () => {
     removeArticles()
     try {
       entertainment.data.data.forEach(entertainmentArticle => {
-        let container = document.querySelector('.all-articles-container')
-        let articleContainer = document.createElement('div')
-        container.append(articleContainer)
-        let title = document.createElement('h1')
-        title.textContent = entertainmentArticle.title
-        articleContainer.append(title)
-        let description = document.createElement('p')
-        description.textContent = entertainmentArticle.description
-        articleContainer.append(description)
-        let image = document.createElement('img')
-        image.src = entertainmentArticle.image_url
-        articleContainer.append(image)
-        let url = document.createElement('url')
-        url.textContent = entertainmentArticle.url
-        articleContainer.append(url)
+      let container = document.querySelector('.all-articles-container')
+      let articleContainer = document.createElement('div')
+      articleContainer.classList.add('article-container')
+      container.append(articleContainer)
+
+      let image = document.createElement('img')
+      image.src = entertainmentArticle.image_url
+      image.classList.add('image-custom')
+      articleContainer.append(image)
+
+      let title = document.createElement('h1')
+      title.textContent = entertainmentArticle.title
+      title.classList.add('title-custom')
+      articleContainer.append(title)
+
+      let description = document.createElement('p')
+      description.textContent = entertainmentArticle.description
+      description.classList.add('description-custom')
+      articleContainer.append(description)
+
+      let button = document.createElement('button')
+      button.classList.add('button-style')
+      articleContainer.append(button)
+      
+      let a = document.createElement('a')
+      a.classList.add('readmore-button-text')
+      a.href = entertainmentArticle.url
+      let link = document.createTextNode('Read More')
+      button.append(a)
+      a.appendChild(link)
       })
     } catch (error) {
       console.log(error)
     }
   }
-  let entertainmentButton = document.querySelector('.entertainment')
-  entertainmentButton.addEventListener('click', getEntertainment)
+let entertainmentButton = document.querySelector('.entertainment')
+entertainmentButton.addEventListener('click', getEntertainment)
+  
+
   //TRAVEL
   const getTravel = async () => {
     let travel = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw&language=en&categories=travel')
@@ -80,27 +105,44 @@ const removeArticles = () => {
     try {
       travel.data.data.forEach(travelArticle => {
         let container = document.querySelector('.all-articles-container')
-        let articleContainer = document.createElement('div')
-        container.append(articleContainer)
-        let title = document.createElement('h1')
-        title.textContent = travelArticle.title
-        articleContainer.append(title)
-        let description = document.createElement('p')
-        description.textContent = travelArticle.description
-        articleContainer.append(description)
-        let image = document.createElement('img')
-        image.src = travelArticle.image_url
-        articleContainer.append(image)
-        let url = document.createElement('url')
-        url.textContent = foodArticle.url
-        articleContainer.append(url)
+      let articleContainer = document.createElement('div')
+      articleContainer.classList.add('article-container')
+      container.append(articleContainer)
+
+      let image = document.createElement('img')
+      image.src = travelArticle.image_url
+      image.classList.add('image-custom')
+      articleContainer.append(image)
+
+      let title = document.createElement('h1')
+      title.textContent = travelArticle.title
+      title.classList.add('title-custom')
+      articleContainer.append(title)
+
+      let description = document.createElement('p')
+      description.textContent = travelArticle.description
+      description.classList.add('description-custom')
+      articleContainer.append(description)
+
+      let button = document.createElement('button')
+      button.classList.add('button-style')
+      articleContainer.append(button)
+      
+      let a = document.createElement('a')
+      a.classList.add('readmore-button-text')
+      a.href = travelArticle.url
+      let link = document.createTextNode('Read More')
+      button.append(a)
+        a.appendChild(link)
       })
     } catch (error) {
       console.log(error)
     }
   }
-  let travelButton = document.querySelector('.travel')
-  travelButton.addEventListener('click', getTravel)
+let travelButton = document.querySelector('.travel')
+travelButton.addEventListener('click', getTravel)
+  
+
   //FOOD
   const getFood = async () => {
     let food = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=lP5M2Cl2kxTGETo0jr47ofrTkKI0F36z4lIzIBhw&language=en&categories=food')
@@ -109,20 +151,35 @@ const removeArticles = () => {
     try {
       food.data.data.forEach(foodArticle => {
         let container = document.querySelector('.all-articles-container')
-        let articleContainer = document.createElement('div')
-        container.append(articleContainer)
-        let title = document.createElement('h1')
-        title.textContent = foodArticle.title
-        articleContainer.append(title)
-        let description = document.createElement('p')
-        description.textContent = foodArticle.description
-        articleContainer.append(description)
-        let image = document.createElement('img')
-        image.src = foodArticle.image_url
-        articleContainer.append(image)
-        let url = document.createElement('url')
-        url.textContent = foodArticle.url
-        articleContainer.append(url)
+      let articleContainer = document.createElement('div')
+      articleContainer.classList.add('article-container')
+      container.append(articleContainer)
+
+      let image = document.createElement('img')
+      image.src = foodArticle.image_url
+      image.classList.add('image-custom')
+      articleContainer.append(image)
+
+      let title = document.createElement('h1')
+      title.textContent = foodArticle.title
+      title.classList.add('title-custom')
+      articleContainer.append(title)
+
+      let description = document.createElement('p')
+      description.textContent = foodArticle.description
+      description.classList.add('description-custom')
+      articleContainer.append(description)
+
+      let button = document.createElement('button')
+      button.classList.add('button-style')
+      articleContainer.append(button)
+      
+      let a = document.createElement('a')
+      a.classList.add('readmore-button-text')
+      a.href = foodArticle.url
+      let link = document.createTextNode('Read More')
+      button.append(a)
+      a.appendChild(link)
       })
     } catch (error) {
       console.log(error)
